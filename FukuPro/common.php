2c812545c7068e93dcd1802a1044eb455ddbbb1f
yoshida1992R <ryo.megane0925@gmail.com>
2015-06-19 11:37:32 +0900

    こもｎ

---
+++ FukuPro/common.php
@@ -0,0 +1,27 @@
+<?php
+  session_start();
+
+  function connect() {
+  	$dsn = 'mysql:dbname=tumtum;host=localhost';
+  	$user = 'root';
+  	$password = 'root';
+    return new PDO($dsn, $user, $password);
+  }
+  
+  function connect2(){
+  	$con = mysql_connect ( "localhost", "root", "root" );
+  	if (! $con) {
+  		print ("MySQLへの接続に失敗しました") ;
+  		exit ();
+  	}
+  	if (! mysql_select_db ( "tumtum" )) {
+  		print ("データベースへの接続に失敗しました") ;
+  		exit ();
+  	}
+  	return  $con;
+  }
+
+  function img_tag($img_id) {
+    return '<img src="get_image.php?key=' . $img_id . '"alt="">';
+  }
+?>
---
+++ FukuPro/index.php
@@ -0,0 +1,11 @@
+<?php
+  require 'common.php';
+  $pdo = connect();
+  $st = $pdo->query("SELECT * FROM goods");
+  $goods = $st->fetchAll();
+  if($_SESSION['member_name']!= null){
+  	require 'l_index.php';
+  }else{
+  require 't_index.php';
+  }
+?>
