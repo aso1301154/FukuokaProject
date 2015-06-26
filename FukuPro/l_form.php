9fbd26d34026a9ba07d6abf50ddbd5bc2256564b
yoshida1992R <ryo.megane0925@gmail.com>
2015-06-26 10:32:48 +0900

    色々追加

---
+++ FukuPro/l_form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta content="utf-8">
+<title>ログイン画面</title>
+</head>
+<body>
+<form action="login.php" method="post">
+メールアドレス
+<input type="text" name="mail">
+パスワード
+<input type="password" name="password">
+<input type="submit" value="ログイン">
+</form>
+</body>
+</html>
+<?php
+?>
---
+++ FukuPro/login.php
@@ -0,0 +1,23 @@
+<?php
+require 'common.php';
+
+$mail = $_POST ["mail"];
+$pass = $_POST ["password"];
+$login = connect2();
+$sql = "SELECT member_id,password,member_name FROM member WHERE mail='$mail'";
+$result = mysql_query ( $sql );
+if (! $result) {
+	//print ("SQLの実行に失敗しました<BR>") ;
+	//print (mysql_errno () . ": " . mysql_error () . "<BR>") ;
+	//exit ();
+}
+$row = mysql_fetch_array ( $result );
+if ($pass == $row['password']) {
+	$_SESSION['member_id'] = $row['member_id'];
+	$_SESSION['member_name'] = $row['member_name'];
+	header('location: index.php');
+	exit;
+}else {
+	print ('IDかパスワードが違うよ！');
+}
+?>
