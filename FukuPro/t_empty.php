bba045f8b8fe3e8b62dfce9dc417b09933af9833
yoshida1992R <ryo.megane0925@gmail.com>
2015-07-01 13:06:40 +0900

    画像認証できた

--- FukuPro/t_empty.php
+++ FukuPro/t_empty.php
@@ -1,3 +1,5 @@
+<?php session_start()?>
+
 <html>
 <head>
 <meta charset="utf-8">
@@ -9,18 +11,19 @@
 	<?php if ($error)echo "<span class=\"error\">$error</span>"?>
 	<form action="empty.php" method="post">
 		<p>
-			お名前<br> <input type="text" name="name" value="<?php echo $name ?>">
+			お名前<br> <input type="text" name="name"
+				value="&lt;?php echo $name ?&gt;">
 		</p>
 		<p>
 			フリガナ<br> <input type="text" name="furigana"
-				value="<?php echo $furigana ?>">
+				value="&lt;?php echo $furigana ?&gt;">
 		</p>
 		<p>
 			生年月日<br> <input type="number" name="year" min="1900" max="2016"
-				value="<?php echo $year ?>"> 年 <input type="number" name="month"
-				min="1" max="12" value="<?php echo $month ?>"> 月 <input
-				type="number" name="日" min="1" max="31" value="<?php echo $day ?>">
-			日
+				value="&lt;?php echo $year ?&gt;"> 年 <input type="number"
+				name="month" min="1" max="12" value="&lt;?php echo $month ?&gt;"> 月
+			<input type="number" name="日" min="1" max="31"
+				value="&lt;?php echo $day ?&gt;"> 日
 		</p>
 		<p>
 			性別<br> <input type="radio" name="sex" value="1">男性 <input
@@ -29,27 +32,27 @@
 		</p>
 		<p>
 			ご住所<br> <input type="text" name="address" size="60"
-				value="<?php echo $address ?>">
+				value="&lt;?php echo $address ?&gt;">
 		</p>
 		<p>
-			電話番号<br> <input type="text" name="tel" value="<?php echo $tel ?>">
+			電話番号<br> <input type="text" name="tel"
+				value="&lt;?php echo $tel ?&gt;">
 		</p>
 		<p>
 			メールアドレス<br> <input type="text" name="mail"
-				value="<?php echo $mail ?>">
+				value="&lt;?php echo $mail ?&gt;">
 		</p>
 
 		<p>
 			画像認証 <input type="text" name="captcha_code"
-				value="<?php echo h($error);?>"> <img id="captcha"
-				src="./securimage/securimage_show.php" alt="CAPTCHA Image" /> 
-				<a
+				value="&lt;?php echo h($error);?&gt;"> <img id="captcha"
+				src="./securimage/securimage_show.php" alt="CAPTCHA Image" /> <a
 				href="#"
 				onclick="document.getElementById('captcha').src = './securimage/securimage_show.php?' + Math.random(); return false">画像変更</a>
 		</p>
 		<p>
-				<input type="submit" name="submit" value="購入">
-			</p>
+			<input type="submit" name="submit" value="購入">
+		</p>
 
 	</form>
 </body>
