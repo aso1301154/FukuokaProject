0e78a7ad3924cb478cc70da6643e42924decb8fa
yoshida1992R <ryo.megane0925@gmail.com>
2015-07-03 10:16:32 +0900

    画像認証

--- FukuPro/t_empty.php
+++ FukuPro/t_empty.php
@@ -12,18 +12,18 @@
 	<form action="empty.php" method="post">
 		<p>
 			お名前<br> <input type="text" name="name"
-				value="&lt;?php echo $name ?&gt;">
+				value="<?php echo $name ?>">
 		</p>
 		<p>
 			フリガナ<br> <input type="text" name="furigana"
-				value="&lt;?php echo $furigana ?&gt;">
+				value="<?php echo $furigana ?>">
 		</p>
 		<p>
 			生年月日<br> <input type="number" name="year" min="1900" max="2016"
-				value="&lt;?php echo $year ?&gt;"> 年 <input type="number"
-				name="month" min="1" max="12" value="&lt;?php echo $month ?&gt;"> 月
+				value="<?php echo $year ?>"> 年 <input type="number"
+				name="month" min="1" max="12" value="<?php echo $month ?>"> 月
 			<input type="number" name="日" min="1" max="31"
-				value="&lt;?php echo $day ?&gt;"> 日
+				value="<?php echo $day ?>"> 日
 		</p>
 		<p>
 			性別<br> <input type="radio" name="sex" value="1">男性 <input
@@ -32,23 +32,22 @@
 		</p>
 		<p>
 			ご住所<br> <input type="text" name="address" size="60"
-				value="&lt;?php echo $address ?&gt;">
+				value="<?php echo $address ?>">
 		</p>
 		<p>
 			電話番号<br> <input type="text" name="tel"
-				value="&lt;?php echo $tel ?&gt;">
+				value="<?php echo $tel ?>">
 		</p>
 		<p>
 			メールアドレス<br> <input type="text" name="mail"
-				value="&lt;?php echo $mail ?&gt;">
+				value="<?php echo $mail ?>">
 		</p>
 
 		<p>
-			画像認証 <input type="text" name="captcha_code"
-				value="&lt;?php echo h($error);?&gt;"> <img id="captcha"
-				src="./securimage/securimage_show.php" alt="CAPTCHA Image" /> <a
-				href="#"
-				onclick="document.getElementById('captcha').src = './securimage/securimage_show.php?' + Math.random(); return false">画像変更</a>
+			画像認証
+			 <input type="text" name="captcha_code"value=''> <br>
+				<img id="captcha" src="./securimage/securimage_show.php" alt="CAPTCHA Image" /> 
+				<a href="#" onclick="document.getElementById('captcha').src = './securimage/securimage_show.php?' + Math.random(); return false">画像変更</a>
 		</p>
 		<p>
 			<input type="submit" name="submit" value="購入">
